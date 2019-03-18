2c1fdaaee31c6d25d3e0e4043adbd6d5bcf0a690
jennifer <jlesteshanby@icloud.com>
2019-03-18 13:26:47 -0400

    initial

---
+++ src/test/java/Group2/Album/Collection/AlbumCollectionApplicationTests.java
@@ -0,0 +1,16 @@
+package Group2.Album.Collection;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AlbumCollectionApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
