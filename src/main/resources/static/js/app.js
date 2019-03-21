747eb925452969e720c7e98098384b6e8b1485a8
akjeffers <aliciakjeffers@live.com>
2019-03-21 14:06:51 -0400

    fixed-javascript

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -6,15 +6,30 @@ fetch('/artists')
 	.then(response => response.json())
 	.then(artist => {
 		let content = `<ul class="artists">`
-		artist.forEach((artist) => {
-			// Make sure to change variable name in Java 
-			content += `<li>${artist.firstName} ${artist.lastName}: 
-			${artist.artistImage}: 
-			${artist.age}: 
-			${artist.hometown}: 
-			${artist.rating}
-			</li>`;
-		})
+		artist.forEach(artist => { 
+			content += `
+			<li class="artist">
+				<h3 class="artist__name">${artist.firstName} ${artist.lastName}</h3> 
+				<img src="${artist.artistImage}" alt="Arttist Image"> 
+				<h4>${artist.age}</h4> 
+				<h4>${artist.hometown}</h4> 
+				<h4>${artist.rating}</h4>
+				<ul class="albums">
+					${artist.allAlbums
+						.map(album => {
+							return `
+							<li class="album">
+								<h5 class="album__title">${album.albumTitle}<h5>
+								<img src="${album.albumImage}" alt="Album Image"/>
+								<h5 class="album__rating">${album.rating}</h5>
+							</li>	  
+						`;
+						})
+					.join("")}
+				</ul>
+			</li>
+		  `;
+		});
 		content += `</ul>`;
 		app.innerHTML = content;
 	})
