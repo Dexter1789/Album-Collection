bdc92487ca9fe48af8656f2a2887fed9c74e29ac
jessicawright <wright.983@gmail.com>
2019-03-22 11:26:51 -0400

    added artist PostMapping

--- src/main/java/Group2/Album/Collection/Controllers/ArtistController.java
+++ src/main/java/Group2/Album/Collection/Controllers/ArtistController.java
@@ -4,7 +4,11 @@ import java.util.Collection;
 
 import javax.annotation.Resource;
 
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,6 +24,21 @@ public class ArtistController {
 
 	@GetMapping("")
 	public Collection<Artist> getAllArtists() {
+		return (Collection<Artist>) artistRepo.findAll();
+	}
+	
+	@PostMapping("/add")
+	public Collection<Artist> addArtist(@RequestBody String body) throws JSONException {
+		JSONObject json = new JSONObject(body);
+		String firstName = json.getString("firstName");
+		String lastName = json.getString("lastName");
+		String artistImage = json.getString("artistImage");
+		String age = json.getString("age");
+		String hometown = json.getString("hometown");
+		int rating = Integer.parseInt(json.getString("rating"));
+		
+		artistRepo.save(new Artist(firstName, lastName, artistImage, age, hometown, rating));
+		
 		return (Collection<Artist>) artistRepo.findAll();
 	}
 }
