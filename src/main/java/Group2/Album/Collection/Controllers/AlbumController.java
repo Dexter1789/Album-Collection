0f0023507cd8c0c75fd2b73c5a8b6c8082dd76c8
jessicawright <wright.983@gmail.com>
2019-03-27 18:09:38 -0400

    add albums and songs

--- src/main/java/Group2/Album/Collection/Controllers/AlbumController.java
+++ src/main/java/Group2/Album/Collection/Controllers/AlbumController.java
@@ -4,12 +4,18 @@ import java.util.Collection;
 
 import javax.annotation.Resource;
 
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import Group2.Album.Collection.models.Album;
+import Group2.Album.Collection.models.Artist;
 import Group2.Album.Collection.repositories.AlbumRepository;
+import Group2.Album.Collection.repositories.ArtistRepository;
 
 @RestController
 @RequestMapping("/albums")
@@ -17,9 +23,23 @@ public class AlbumController {
 	
 	@Resource
 	AlbumRepository albumRepo;
+	
+	@Resource
+	ArtistRepository artistRepo;
 
 	@GetMapping("")
 	public Collection<Album> getAllAlbums() {
 		return (Collection<Album>) albumRepo.findAll();
+	}
+	
+	@PostMapping("/add")
+	public Collection<Artist> addAlbum(@RequestBody String body) throws JSONException {
+		JSONObject json = new JSONObject(body);
+		String albumTitle = json.getString("albumTitle");
+		String albumImage = json.getString("albumImage");
+		int rating = Integer.parseInt(json.getString("rating"));
+		Artist artist = artistRepo.findByLastName(json.getString("artist"));
+		albumRepo.save(new Album(albumTitle, albumImage, rating, artist));
+		return (Collection<Artist>) artistRepo.findAll();
 	}
 }
